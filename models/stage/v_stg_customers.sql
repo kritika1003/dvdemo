90f2308c9d9f6bc48e4c15558243de08074ad116
Avinashreddy29 <54271392+Avinashreddy29@users.noreply.github.com>
2021-04-02 12:25:39 +0530

    Update v_stg_customers.sql

--- models/stage/v_stg_customers.sql
+++ models/stage/v_stg_customers.sql
@@ -28,27 +28,10 @@ hashed_columns:
 {%- do log("hashed_columns: " ~ hashed_columns, true) %}
 
 
-
-
-
-
-
     {{ dbtvault.stage(include_source_columns=true,
-
-
-
                       source_model=source_model,
-
-
-
                       derived_columns=derived_columns,
-
-
-
                       hashed_columns=hashed_columns,
-
-
-
                       ranked_columns=none) }}
 
 

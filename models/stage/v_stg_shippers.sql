ef424a497d32a2f57b0d58adc720ba2e1e110735
Avinashreddy29 <54271392+Avinashreddy29@users.noreply.github.com>
2021-04-02 12:24:08 +0530

    Update v_stg_shippers.sql

--- models/stage/v_stg_shippers.sql
+++ models/stage/v_stg_shippers.sql
@@ -26,21 +26,9 @@ hashed_columns:
 
 
     {{ dbtvault.stage(include_source_columns=true,
-
-
-
                       source_model=source_model,
-
-
-
                       derived_columns=derived_columns,
-
-
-
                       hashed_columns=hashed_columns,
-
-
-
                       ranked_columns=none) }}
 
 
